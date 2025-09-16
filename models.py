f9ca09bd0659367527a592551584602c401d43e2
Adam Skuta <adam.skuta@ait.ac.at>
2025-09-16 17:19:19 +0200

    Started first MLP

--- models.py
+++ models.py
@@ -1,3 +1,24 @@
+from typing import List
+
 import jax
 from jax import numpy as jnp
 from flax import linen as nn
+
+
+class MyMLP(nn.Module):
+    layers: int
+    neurons: int
+
+    @nn.compact
+    def __call__(self, x):
+        for l in range(self.layers-1):
+            x = nn.Dense(self.neurons, use_bias=False)(x)
+            x = nn.relu(x)
+        y = nn.Dense(self.neurons, use_bias=False)(x)
+        return y
+    
+
+if __name__ == "__main__":
+    model = MyMLP(1, 30)
+    rng = jax.random.PRNGKey(0)
+    print(model.tabulate(rng, jnp.ones((1, 10))))
