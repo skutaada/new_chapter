c74cfc2d72d3880c1d0ca0fa39b358aa60bb8442
Adam Skuta <adam.skuta@ait.ac.at>
2025-09-17 14:30:11 +0200

    Added benchmark for small models

--- benchmark.py
+++ benchmark.py
@@ -2,13 +2,15 @@ import time
 import json
 import argparse
 import statistics as stat
+from typing import Tuple
 
 import jax
 from jax import numpy as jnp
+from flax import linen as nn
 from tqdm import tqdm
 from spu.utils import distributed as ppd
 
-from models import MyMLP
+from models import MLP, LSTM, CNN
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--num-epochs", type=int)
@@ -22,69 +24,100 @@ def count_parameters(params):
     return sum(jnp.size(p) for p in jax.tree_util.tree_leaves(params))
 
 
-def run(args):
+def benchmark_model(model_def: nn.Module, input_shape: Tuple[int], runs=1000):
+    rng = jax.random.PRNGKey(0)
+    x = jnp.ones(input_shape)
+    params = model_def.init(rng, x)
+    params_s = ppd.device("P2")(lambda x: x)(params)
+
+    num_params = count_parameters(params)
+
+    for _ in range(10):
+        x = jax.random.normal(rng, input_shape)
+        x_s = ppd.device("P1")(lambda x: x)(x)
+        model_def.apply(params, x)
+        y_s = ppd.device("SPU")(model_def.apply)(params_s, x_s)
+        y = ppd.get(y_s)
+
+    time_s = []
+    time_p = []
+    for _ in range(runs):
+        x = jax.random.normal(rng, input_shape)
+        x_s = ppd.device("P1")(lambda x: x)(x)
+        start = time.time()
+        _ = model_def.apply(params, x)
+        time_p.append(start - time.time())
+
+        start = time.time()
+        y_s = ppd.device("SPU")(model_def.apply)(params_s, x_s)
+        time_s.append(start - time.time())
+        y = ppd.get(y_s)
+
+    stats = {
+        "num_params": num_params,
+        "mean_p": stat.mean(time_p),
+        "stdev_p": stat.stdev(time_p),
+        "mean_s": stat.mean(time_s),
+        "stdev_s": stat.stdev(time_s)
+    }
+
+    return stats
+
+
+mlp_configs = {
+    "Very Wide Shallow": [1000],
+    "Wide": [500, 500],
+    "Balanced": [250]*4,
+    "Deep": [125]*8,
+    "Very Deep Narrow": [62]*16
+}
+mlp_inputs = [(1, 128), (1, 512), (1, 1024)]
+
+lstm_configs = {
+    "Very Wide Shallow": [128],
+    "Wide": [90, 90],
+    "Balanced": [75, 75, 75],
+    "Deep": [64]*4,
+    "Very Deep Narrow": [50]*6
+}
+lstm_inputs = [(1, 16, 32), (1, 32, 64), (1, 64, 128)]
+
+cnn_configs = {
+    "Very Wide Shallow": [128, 128, 128],
+    "Wide": [64]*6,
+    "Balanced": [45]*9,
+    "Deep": [32]*12,
+    "Very Deep Narrow": [22]*18
+}
+cnn_inputs = [(1, 32, 32, 3), (1, 64, 64, 3), (1, 128, 128, 3)]
+
+
+def main(args):
     with open(args.config, 'r') as f:
         conf = json.load(f)
 
     ppd.init(conf["nodes"], conf["devices"])
-
-    rng = jax.random.PRNGKey(0)
-
-    results = []
-
-    nl_list = [1, 10, 100]
-    nn_list = [10, 100, 1000]
-    i_shapes = [[1, 10], [1, 50], [1, 100]]
-
-    total_iters = len(nl_list) * len(nn_list) * len(i_shapes) * args.num_epochs
-    pbar = tqdm(total=total_iters)
-
-    for nl in nl_list:
-        for nn in nn_list:
-            for i_shape in i_shapes:
-                model = MyMLP(nl, nn)
-                params = model.init(rng, jnp.ones(i_shape))
-                params_s = ppd.device("P2")(lambda x: x)(params)
-                r = {
-                    "n_params": count_parameters(params),
-                    "i_shape": i_shape,
-                    "nn": nn,
-                    "nl": nl,
-                }
-                time_p = []
-                time_s = []
-                for _ in range(args.num_epochs):
-                    x = jax.random.normal(rng, i_shape)
-                    x_s = ppd.device("P1")(lambda x: x)(x)
-
-                    start = time.time()
-                    model.apply(params, x)
-                    end = time.time()
-                    time_p.append(end-start)
-
-                    start = time.time()
-                    y_s = ppd.device("SPU")(model.apply)(params_s, x_s)
-                    end = time.time()
-                    ppd.get(y_s)
-                    time_s.append(end-start)
-
-                    pbar.set_description(f"#layers={nl}, #neurons={nn}, shape={i_shape}")
-                    pbar.update(1)
-                r["mean_p"] = stat.mean(time_p)
-                r["mean_s"] = stat.mean(time_s)
-                r["stdev_p"] = stat.stdev(time_p)
-                r["stdev_s"] = stat.stdev(time_s)
-                results.append(r)
-    pbar.close()
-            
-
-
+    full_stats = []
+    for model_name, configs, inputs, cls in [
+        ("MLP", mlp_configs, mlp_inputs, MLP),
+        ("LSTM", lstm_configs, lstm_inputs, LSTM),
+        ("CNN", cnn_configs, cnn_inputs, CNN)
+    ]:
+        print(f"\n===== {model_name} (Small size) =====")
+        for ratio_name, config in configs.items():
+            for in_shape in inputs:
+                model = cls(config)
+                stats = benchmark_model(model, in_shape, args.num_epochs)
+                stats["type"] = model_name
+                stats["input_shape"] = in_shape
+                stats["model_config"] = config
+                full_stats.append(stats)
+    
     with open(args.results, 'w') as f:
-        json.dump(results, f)
+        json.dump(full_stats, f)
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    run(args)
+    main(args)
 
-    
--- models.py
+++ models.py
@@ -5,20 +5,42 @@ from jax import numpy as jnp
 from flax import linen as nn
 
 
-class MyMLP(nn.Module):
-    layers: int
-    neurons: int
+class MLP(nn.Module):
+    features: list[int]
 
     @nn.compact
     def __call__(self, x):
-        for l in range(self.layers-1):
-            x = nn.Dense(self.neurons, use_bias=False)(x)
+        for feat in self.features:
+            x = nn.Dense(feat)(x)
             x = nn.relu(x)
-        y = nn.Dense(self.neurons, use_bias=False)(x)
-        return y
+        return nn.Dense(10)(x)
     
 
-if __name__ == "__main__":
-    model = MyMLP(1, 30)
-    rng = jax.random.PRNGKey(0)
-    print(model.tabulate(rng, jnp.ones((1, 10))))
+class LSTM(nn.Module):
+    hidden_sizes: list[int]
+
+    @nn.compact
+    def __call__(self, x):
+        carry = None
+        for hs in self.hidden_sizes:
+            cell = nn.LSTMCell(hs)
+            rnn = nn.RNN(cell)
+            outputs = []
+            for t in range(x.shape[1]):
+                y = rnn(x[:,t,:])
+                outputs.append(y)
+            x = jnp.stack(outputs, axis=1)
+        x = x[:, -1, :]
+        return nn.Dense(10)(x)
+    
+
+class CNN(nn.Module):
+    channels: list[int]
+
+    @nn.compact
+    def __call__(self, x):
+        for ch in self.channels:
+            x = nn.Conv(ch, kernel_size=(3, 3))(x)
+            x = nn.max_pool(x, (3, 3))
+            x = nn.relu(x)
+        return nn.Dense(10)(x)
